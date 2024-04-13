9fe960281f560fdf738cd36d7ecbecb0735840d6
Sofia Razuvalova <38169389+SofiazaR@users.noreply.github.com>
2024-04-13 18:44:00 +0300

    Update page.tsx

--- app/home/(overview)/page.tsx
+++ app/home/(overview)/page.tsx
@@ -2,13 +2,6 @@
 import { lusitana } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 import Prompt from '@/app/ui/prompt-input';
-import Output from '@/app/ui/preview';
-import Input from '@/app/ui/code-view';
-import {
-  GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
-} from "@google/generative-ai"
  
 export const metadata: Metadata = {
   title: 'Generate',
